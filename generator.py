fc96dbaa08924d927537665c53d8954606f80dab
MrPawelas <pawelek.miecz@gmail.com>
2019-10-21 10:35:27 +0200

    Update generator.py
    trochę lepszy słaby generator

--- generator.py
+++ generator.py
@@ -4,10 +4,17 @@ n=int(input("Podaj ilosc wierzcholkow"))
 x=[]
 y=[]
 file = open("testfile.txt","w")
+
 for i in range (n):
-   x.append(random.randint(1,100))
-   y.append(random.randint(1,100))
+
+    new_x=random.randint(1,100)
+    new_y=random.randint(1,100)
+    while x.__contains__(new_x) and y.__contains__(new_y):
+        new_x = random.randint(1, 100)
+        new_y = random.randint(1, 100)
+    x.append(new_x)
+    y.append(new_y)
+
 for i in range (n):
     napis= str(i+1)+" "+str(x[i])+" "+str(y[i])+"\n"
     file.write(str(napis))
-
